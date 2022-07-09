ece35066182a4a2b553fdb4b5bd5b053e6404f6c
SXT <108885177+littlekid9@users.noreply.github.com>
2022-07-09 17:04:52 +0800

    用户数据库

---
+++ dj_amin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `dj_amin` (`did`, `dname`, `dpwd`, `dphone`, `demail`) VALUES ('1', 'dingding', '123456', '18512355555', '123@qq.com');
+INSERT INTO `dj_amin` (`did`, `dname`, `dpwd`, `dphone`, `demail`) VALUES ('2', 'dangdang', '123456', '12345678955', '111@qq.com');
+INSERT INTO `dj_amin` (`did`, `dname`, `dpwd`, `dphone`, `demail`) VALUES ('3', NULL, NULL, NULL, NULL);
---
+++ dj_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `dj_user` (`did`, `dphone`, `demail`, `dpwd`) VALUES (1, '13171831633', '1031124494@qq.com', 'abc123456');
+INSERT INTO `dj_user` (`did`, `dphone`, `demail`, `dpwd`) VALUES (2, '13552425659', '13552425659@qq.com', 'abc111111');
+INSERT INTO `dj_user` (`did`, `dphone`, `demail`, `dpwd`) VALUES (3, '13185266654', '13185266654@qq.com', 'aaa123456');
+INSERT INTO `dj_user` (`did`, `dphone`, `demail`, `dpwd`) VALUES (4, '13174859595', '13174859595@qq.com', 'bbb123456');
+INSERT INTO `dj_user` (`did`, `dphone`, `demail`, `dpwd`) VALUES (5, '13152524545', '13152524545@qq.com', NULL);
